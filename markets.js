b7f6cc2f7811dcc3ed89eaa8826f5000c9acd698
SpinnakerFx <72513708+fxspinnaker@users.noreply.github.com>
2020-10-07 16:42:00 +0000

    Add files via upload

---
+++ markets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        nameBinance: "ETHUSDC",
+        nameLoopring: "ETH-USDC",
+        amount: 0.02,
+        spread: 2.0,
+        additionalDelta: 0.02,
+        idealInventoryAmount: 50.0
+    }
+]
